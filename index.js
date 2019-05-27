bd0d6c355a48fa843730f33ff902d436fed038a3
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2019-05-27 18:26:30 +0200

    remove testing code

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-require('fs').writeFileSync('certbase64.txt', require('fs').readFileSync('test/certificate.p12').toString('base64'))
-
-var main = require('./lib/main.js')
-var config = require('./jsreport.config.js')
+const main = require('./lib/main.js')
+const config = require('./jsreport.config.js')
 
 module.exports = function (options) {
   config.options = options
