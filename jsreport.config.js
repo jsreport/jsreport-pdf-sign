546bb838095a290fa18d98329b10457f76745093
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2019-05-27 16:46:05 +0200

    update package lock and add secret to the config

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   'name': 'pdf-sign',
   'dependencies': ['templates', 'assets'],
-  'main': 'lib/main.js'
+  'main': 'lib/main.js',
+  'optionsSchema': {
+    extensions: {
+      'pdf-sign': {
+        type: 'object',
+        properties: {
+          secret: { type: 'string' }
+        }
+      }
+    }
+  }
 }
